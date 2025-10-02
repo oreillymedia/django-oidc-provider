e358c69c93850e1481fe7cc8c25c42bd2ddea81a
Ben Davis <bendavis78@gmail.com>
2025-10-02 16:55:41 -0500

    UA-4672 | Bring in latest changes from upstream, bump to 0.9.0+orm.1

--- oidc_provider/lib/endpoints/token.py
+++ oidc_provider/lib/endpoints/token.py
@@ -11,7 +11,10 @@ from oidc_provider import settings
 from oidc_provider.lib.errors import TokenError
 from oidc_provider.lib.errors import UserAuthError
 from oidc_provider.lib.utils.oauth2 import extract_client_auth
+from oidc_provider.lib.utils.sanitization import sanitize_client_id
+from oidc_provider.lib.utils.token import create_id_token
 from oidc_provider.lib.utils.token import create_token
+from oidc_provider.lib.utils.token import encode_id_token
 from oidc_provider.models import Client
 from oidc_provider.models import Code
 from oidc_provider.models import Token
@@ -32,7 +35,7 @@ class TokenEndpoint(object):
     def _extract_params(self):
         client_id, client_secret = extract_client_auth(self.request)
 
-        self.params["client_id"] = client_id
+        self.params["client_id"] = sanitize_client_id(client_id)
         self.params["client_secret"] = client_secret
         self.params["redirect_uri"] = self.request.POST.get("redirect_uri", "")
         self.params["grant_type"] = self.request.POST.get("grant_type", "")
@@ -257,7 +260,7 @@ class TokenEndpoint(object):
             "refresh_token": token.refresh_token,
             "token_type": "bearer",
             "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
-            "id_token": self._encode_id_token(id_token_dic, token.client),
+            "id_token": encode_id_token(id_token_dic, token.client),
         }
 
         return dic
@@ -304,7 +307,7 @@ class TokenEndpoint(object):
             "refresh_token": token.refresh_token,
             "token_type": "bearer",
             "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
-            "id_token": self._encode_id_token(id_token_dic, self.token.client),
+            "id_token": encode_id_token(id_token_dic, self.token.client),
         }
 
         return dic
@@ -338,7 +341,7 @@ class TokenEndpoint(object):
             "refresh_token": token.refresh_token,
             "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
             "token_type": "bearer",
-            "id_token": self._encode_id_token(id_token_dic, token.client),
+            "id_token": encode_id_token(id_token_dic, token.client),
             "scope": " ".join(token.scope),
         }
 
